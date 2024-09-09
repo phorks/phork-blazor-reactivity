33b817b4327560f970b4dce856aa7456eef19fc4
Denny09310 <k.denny2000@gmail.com>
2024-09-09 08:45:15 +0200

    feat: added simple reactive layout component

---
+++ src/Phork.Blazor.Reactivity/ReactiveLayoutComponentBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Phork.Blazor;
+
+/// <summary>
+/// Provides the base class for reactive components. Alternatively, components may implement <see
+/// cref="IReactiveComponent"/> directly.
+/// </summary>
+public abstract class ReactiveLayoutComponentBase : ReactiveComponentBase
+{
+    internal const string BodyPropertyName = nameof(Body);
+
+    /// <summary>
+    /// Gets the content to be rendered inside the layout.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? Body { get; set; }
+
+    /// <inheritdoc />
+    // Derived instances of LayoutComponentBase do not appear in any statically analyzable
+    // calls of OpenComponent<T> where T is well-known. Consequently we have to explicitly provide a hint to the trimmer to preserve
+    // properties.
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(LayoutComponentBase))]
+    public override Task SetParametersAsync(ParameterView parameters) => base.SetParametersAsync(parameters);
+}
